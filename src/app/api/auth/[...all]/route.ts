193d24433d15b82c552899b5d57f95a6c5db8061
visatk <visatk29@gmail.com>
2026-05-25 01:18:46 +0600

    Update route.ts

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,7 +1,4 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-// Cloudflare Pages er edge runtime enable kora holo
-export const runtime = "edge";
-
 export const { GET, POST } = toNextJsHandler(auth);
